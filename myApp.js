e0a1db11a4a21be91652f3b97b0c8b81899f7f3d
ddoyediran <ddoyediran@gmail.com>
2022-10-07 01:20:13 +0200

    intial commit

--- myApp.js
+++ myApp.js
@@ -1,8 +1,15 @@
 let express = require("express");
+const path = require("path");
 let app = express();
 
+// app.use("/static", express.static(path.join(__dirname, "/views/index.html")));
+
+// app.get("/", function (req, res) {
+//   res.send("Hello Express");
+// });
+
 app.get("/", function (req, res) {
-  res.send("Hello Express");
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 console.log("Hello World");
