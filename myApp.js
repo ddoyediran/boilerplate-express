78ab45a77b69e669e6a1f3f53f7d250eb6e08436
ddoyediran <ddoyediran@gmail.com>
2022-10-14 21:38:50 +0200

    more commit

--- myApp.js
+++ myApp.js
@@ -1,6 +1,11 @@
 let express = require("express");
 const path = require("path");
 let app = express();
+require("dotenv").config();
+
+//const vee = process.env.MESSAGE_STYLE;
+
+///console.log(vee);
 
 // app.use("/static", express.static(path.join(__dirname, "/views/index.html")));
 
@@ -10,6 +15,15 @@ let app = express();
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/json", function (req, res) {
+  const vee = process.env.MESSAGE_STYLE;
+
+  if (vee.toString() === "uppercase") {
+    res.json({ message: vee.toUpperCase() });
+  }
+  //res.json({ name: "Ade" });
 });
 
 console.log("Hello World");
